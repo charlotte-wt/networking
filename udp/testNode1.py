110f16d0480cb2fefeeffb50d87a73c5f2bd8fe6
charlotte-wt <charlottewee01@gmail.com>
2023-03-03 10:39:56 +0800

    udp pairwise communication

---
+++ udp/testNode1.py
@@ -0,0 +1,67 @@
+#!usr/bin/python
+
+import socket
+import threading
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)      # For UDP
+
+udp_host = "localhost"        # Host IP
+udp_port = 12345              # specified port to connect
+source_ip = "0x1A"
+
+# print type(sock) ============> 'type' can be used to see type
+# of any variable ('sock' here)
+
+sock.bind((udp_host, udp_port))
+
+
+def send_message():
+    
+    while True:
+        try:
+            message = input("\nEnter the text message to send: ")
+            destination_ip = input("Enter the IP of the clients to send the message to:\n1. 0x2A\n2. 0x2B\n")
+            protocol = input("\nPlease enter the protocol of the packet (in an integer):\n0: ping protocol\n1: log protocol\n2: kill protocol\n")
+            
+            ethernet_header = ""
+            IP_header = ""
+
+            if(destination_ip == "0x2A" or destination_ip == "0x2B"):
+                    
+                    IP_header = IP_header + source_ip + destination_ip
+                    
+                    # source_mac = server_mac
+                    # destination_mac = router_mac 
+                    # ethernet_header = ethernet_header + source_mac + destination_mac
+                    
+                    packet = IP_header + protocol + "0x{:02x}".format(len(message)) + message
+                    
+            
+            else:
+                print("Wrong client IP inputted")
+            print("UDP target IP:", udp_host)
+            print("UDP target Port:", 12346)
+
+            # Sending message to UDP server
+            sock.sendto(packet.encode(), (udp_host, 12346))
+
+        except(KeyboardInterrupt, EOFError):
+            print('\n[INFO]: Keyboard Interrupt Received')
+            exit()
+
+def wait_client():
+    while True:
+        try:
+            print("Waiting for client...")
+            data, addr = sock.recvfrom(1024)  # receive data from client
+            print("Received Messages:", data.decode(), " from", addr)
+        except(KeyboardInterrupt, EOFError):
+            print('\n[INFO]: Keyboard Interrupt Received')
+            exit()
+    
+if __name__ == "__main__":
+    x = threading.Thread(target=wait_client)
+    y = threading.Thread(target=send_message)
+    x.start()
+    y.start()
+    
