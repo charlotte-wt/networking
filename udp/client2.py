455a21f5dd32d0b2401e6247374338f7fe830bb3
ys-tx <92923783+ys-tx@users.noreply.github.com>
2023-03-03 16:30:37 +0800

    Created connection between client 2 and 3
    Start the programs in this order:
    1. router.py
    2. client2.py
    3. client3.py

---
+++ udp/client2.py
@@ -0,0 +1,37 @@
+import socket
+import struct
+
+source_ip = "0x2A"
+destination_ip = "0x2B"
+router_ip = "0x21"
+
+arp_table = {
+    '0x1A': 'N1',
+    '0x2A': 'N2',
+    '0x2B': 'N3',
+    '0x11': 'R1',
+    '0x21': 'R2'
+}
+
+# Create a UDP socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Bind the socket to a specific IP address and port number
+client_socket.bind(('127.0.0.1', 2234))
+
+while True:
+    # Send a message to the router
+    message = input("Client 2: ")
+    message_len = len(message)
+
+    ip_packet = bytes.fromhex(source_ip[2:]) + bytes.fromhex(destination_ip[2:]) + struct.pack('B', int(message_len)) + message.encode()
+
+    # to create the ethernet frame
+    source_mac = arp_table.get(source_ip)
+    destination_mac = arp_table.get(router_ip)
+    ethernet_frame = bytes.fromhex(source_mac.encode('ascii').hex()) + bytes.fromhex(destination_mac.encode('ascii').hex()) + ip_packet
+
+    client_socket.sendto(ethernet_frame, ('127.0.0.1', 1234))
+
+    # Receive a message from the router
+    response, _= client_socket.recvfrom(1024)
+    print(f"Client 3: {response.decode()[7:]}")
---
+++ udp/router.py
@@ -0,0 +1,41 @@
+import socket
+
+router2_ip = "0x21"
+
+arp_table = {
+    '0x1A': 'N1',
+    '0x2A': 'N2',
+    '0x2B': 'N3',
+    '0x11': 'R1',
+    '0x21': 'R2'
+}
+
+# Create a UDP socket
+router_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to a specific IP address and port number
+router_socket.bind(('127.0.0.1', 1234))
+
+while True:
+    # Receive a message from either client 1 or client 2
+    ethernet_frame_recv, address = router_socket.recvfrom(1024)
+    decoded_ethernet_frame_recv = ethernet_frame_recv.decode('ascii')
+    print(f"Received message from {address}: {decoded_ethernet_frame_recv}")
+
+    # Forward the message to the other client
+    if address[1] == 2234:
+        # Message came from client 2, forward to client 3
+        destination_ip = "0x2B"
+        new_source_mac = arp_table.get(router2_ip)
+        new_destination_mac = arp_table.get(destination_ip)
+        new_ethernet_frame = bytes.fromhex(new_source_mac.encode('ascii').hex()) + bytes.fromhex(new_destination_mac.encode('ascii').hex()) + ethernet_frame_recv[4:]
+
+        router_socket.sendto(new_ethernet_frame, ('127.0.0.1', 3234))
+    elif address[1] == 3234:
+        # Message came from client 3, forward to client 2
+        destination_ip = "0x2A"
+        new_source_mac = arp_table.get(router2_ip)
+        new_destination_mac = arp_table.get(destination_ip)
+        new_ethernet_frame = bytes.fromhex(new_source_mac.encode('ascii').hex()) + bytes.fromhex(new_destination_mac.encode('ascii').hex()) + ethernet_frame_recv[4:]
+
+        router_socket.sendto(new_ethernet_frame, ('127.0.0.1', 2234))
