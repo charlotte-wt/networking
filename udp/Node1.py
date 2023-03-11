a90610da54ffd5eb0f257db8ba4f5141269d3b54
ys-tx <92923783+ys-tx@users.noreply.github.com>
2023-03-11 10:02:18 +0800

    Updated code to emulate broadcast effect
    Assuming Node 1 wants to communicate with Node 3, run programs in the following steps:
    1. Type `python Router.py` (repeat for Node 2 and 3 on respective terminals)
    2. On Node 1, type `python Node1.py --ping 0x2B`

--- udp/Node1.py
+++ udp/Node1.py
@@ -4,8 +4,8 @@ import socket
 
 arp_cache = []
 
-source_ip = "0x2B"
-source_mac = "N3"
+source_ip = "0x1A"
+source_mac = "N1"
 
 router1_ip = "0x11"
 router2_ip = "0x21"
@@ -19,7 +19,7 @@ parser.add_argument('--arp-a', action='store_true', help="Dispay ARP table")
 
 # Create a UDP socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.bind(('127.0.0.1', 3234))
+client_socket.bind(('127.0.0.1', 5678))
         
 # Parse the command line arguments
 args = parser.parse_args()
@@ -40,14 +40,13 @@ elif (args.ping):
         arp_request = bytes.fromhex(ether_type[2:]) + bytes.fromhex(source_mac.encode('ascii').hex()) + bytes.fromhex(source_ip[2:]) + bytes.fromhex(destination_ip[2:]) + destination_mac.encode()
         
         # send a response message to the sender
-        client_socket.sendto(arp_request, ('127.0.0.1', 1234))
-        print("ARP Request sent from Node 3:", arp_request)
+        client_socket.sendto(arp_request, ('127.0.0.1', 8765))
+        print("ARP Request sent from Node 1:", arp_request)
         
         while True:
         # receive data from the socket
             data, address = client_socket.recvfrom(1024)
             decoded_data = data.decode('ascii')
-
             if (bytes.fromhex("0x0806"[2:]) in data):
                 source_mac_recv = decoded_data[2:4]
                 destination_mac = source_mac_recv
@@ -55,27 +54,27 @@ elif (args.ping):
                 # add mac into arp cache
                 arp_cache.append(destination_mac)
                 print("Connection Established")
-                
-                message = input("Node 3: ")
+
+                message = input("Node 1: ")
                 message_len = len(message)
                 
                 ether_type = "0x0800"
                 ether_frame = bytes.fromhex(ether_type[2:]) + bytes.fromhex(source_mac.encode('ascii').hex()) + bytes.fromhex(source_ip[2:]) + bytes.fromhex(destination_ip[2:]) + destination_mac.encode() + struct.pack('B', int(message_len)) + message.encode()
-                client_socket.sendto(ether_frame, ('127.0.0.1', 1234))
+                client_socket.sendto(ether_frame, ('127.0.0.1', 8765))
 
             else:
                 print(decoded_data[9:])
-                message = input("Node 3: ")
+                message = input("Node 1: ")
                 message_len = len(message)
                 
                 ether_type = "0x0800"
                 ether_frame = bytes.fromhex(ether_type[2:]) + bytes.fromhex(source_mac.encode('ascii').hex()) + bytes.fromhex(source_ip[2:]) + bytes.fromhex(destination_ip[2:]) + destination_mac.encode() + struct.pack('B', int(message_len)) + message.encode()
-                client_socket.sendto(ether_frame, ('127.0.0.1', 1234))
+                client_socket.sendto(ether_frame, ('127.0.0.1', 8765))
 
 else:
     while True:
         # Receive a message from the router
-        response, _ = client_socket.recvfrom(1024)
+        response, _ = client_socket.recvfrom(8765)
         decoded_arp_request_recv = response.decode('ascii')
 
         source_mac_recv = decoded_arp_request_recv[2:4]
@@ -88,17 +87,17 @@ else:
                 ether_type = "0x0806"
                 arp_reply = bytes.fromhex(ether_type[2:]) + bytes.fromhex(source_mac.encode('ascii').hex()) + bytes.fromhex(source_ip[2:]) + bytes.fromhex(source_ip_recv[2:]) + source_mac_recv.encode()
                 print(f"ARP Reply: {arp_reply}")
-                client_socket.sendto(arp_reply, ('127.0.0.1', 1234))
+                client_socket.sendto(arp_reply, ('127.0.0.1', 8765))
             else:
                 print("dropping frame")
 
         else:
             print(decoded_arp_request_recv[9:])
-            message = input("Node 3: ")
+            message = input("Node 1: ")
             message_len = len(message)
             
             ether_type = "0x0800"
             ether_frame = bytes.fromhex(ether_type[2:]) + bytes.fromhex(source_mac.encode('ascii').hex()) + bytes.fromhex(source_ip[2:]) + bytes.fromhex(source_ip_recv[2:]) + source_mac_recv.encode() + struct.pack('B', int(message_len)) + message.encode()
-            client_socket.sendto(ether_frame, ('127.0.0.1', 1234))
+            client_socket.sendto(ether_frame, ('127.0.0.1', 8765))
         
 
--- udp/Router.py
+++ udp/Router.py
@@ -1,41 +1,57 @@
 import socket
+import select
 
+router1_ip = "0x11"
 router2_ip = "0x21"
 
-arp_table = {
-    '0x1A': 'N1',
-    '0x2A': 'N2',
-    '0x2B': 'N3',
-    '0x11': 'R1',
-    '0x21': 'R2'
-}
+# Define the port numbers
+PORT1 = 8765
+PORT2 = 1234
 
-# Create a UDP socket
-router_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Create a UDP socket to receive from Node 2 or 3
+internal_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+internal_sock.bind(('127.0.0.1', PORT2))
+internal_sock.setblocking(0) # make the socket non-blocking
 
-# Bind the socket to a specific IP address and port number
-router_socket.bind(('127.0.0.1', 1234))
+# Create a UDP socket to receive from Node 1
+external_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+external_sock.bind(('0.0.0.0', PORT1))
+external_sock.setblocking(0) # make the socket non-blocking
+
+# Listen for incoming packets on both sockets
+import socket
+import select
 
 while True:
-    # Receive a message from either node 1 or node 2
-    ethernet_frame_recv, address = router_socket.recvfrom(1024)
-    decoded_ethernet_frame_recv = ethernet_frame_recv.decode('ascii')
-    print(f"Received message from {address}: {decoded_ethernet_frame_recv}")
+    # List of sockets to listen for incoming data
+    sockets_list = [internal_sock, external_sock]
 
-    # Forward the message to the other node
-    if address[1] == 2234:
-        # Message came from Node 2, forward to No 3
-        destination_ip = "0x2B"
-        new_source_mac = arp_table.get(router2_ip)
-        new_destination_mac = arp_table.get(destination_ip)
-        new_ethernet_frame = bytes.fromhex(new_source_mac.encode('ascii').hex()) + bytes.fromhex(new_destination_mac.encode('ascii').hex()) + ethernet_frame_recv[4:]
+    # Use select to wait for incoming data on any of the sockets
+    read_sockets, write_sockets, error_sockets = select.select(sockets_list, [], [])
 
-        router_socket.sendto(new_ethernet_frame, ('127.0.0.1', 3234))
-    elif address[1] == 3234:
-        # Message came from node 3, forward to node 2
-        destination_ip = "0x2A"
-        new_source_mac = arp_table.get(router2_ip)
-        new_destination_mac = arp_table.get(destination_ip)
-        new_ethernet_frame = bytes.fromhex(new_source_mac.encode('ascii').hex()) + bytes.fromhex(new_destination_mac.encode('ascii').hex()) + ethernet_frame_recv[4:]
+    for sock in read_sockets:
+        data, addr = sock.recvfrom(1024)
+        from_port = addr[1]
 
-        router_socket.sendto(new_ethernet_frame, ('127.0.0.1', 2234))
+        if (from_port == 5678):
+            if (bytes.fromhex("0x0806"[2:]) in data):   # arp reply-request
+                # print("Broadcast message")
+                sock.sendto(data, ('127.0.0.1', 2234))
+                sock.sendto(data, ('127.0.0.1', 3234))
+            else:
+                if (bytes.fromhex("0x2A"[2:]) in data):                         
+                    sock.sendto(data, ('127.0.0.1', 2234))
+                else:
+                    sock.sendto(data, ('127.0.0.1', 3234))
+        elif (from_port == 2234):
+            if (bytes.fromhex("0x2B"[2:]) in data):
+                sock.sendto(data, ('127.0.0.1', 3234))
+            else:
+                sock.sendto(data, ('127.0.0.1', 5678))
+            # break
+        elif (from_port == 3234):
+            if (bytes.fromhex("0x2A"[2:]) in data):
+                sock.sendto(data, ('127.0.0.1', 2234))
+            else:
+                sock.sendto(data, ('127.0.0.1', 5678))
+        
